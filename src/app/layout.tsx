9747b427e792a4ee02fa9a065f65aa9c2dfff050
kanukuganesh <kanukuganesh@users.noreply.github.com>
2026-09-13 20:15:52 +0530

    Add Google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="bFLSexQvWxNQcgJuR9dnIkYvAERf5YaQIKbEFikuw-w" />
         <link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
