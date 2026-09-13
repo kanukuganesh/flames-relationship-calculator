07a09121e3aee8c6bceb7ea9916d148d03ef5524
kanukuganesh <kanukuganesh@users.noreply.github.com>
2026-09-13 20:29:37 +0530

    Update Google site verification tag for flames143.vercel.app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="bFLSexQvWxNQcgJuR9dnIkYvAERf5YaQIKbEFikuw-w" />
+        <meta name="google-site-verification" content="3umV4mvqVIxo67qWhkMOA27IkZLO1KUxLTnSsj67VmE" />
         <link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
